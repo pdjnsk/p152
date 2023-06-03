c47b3605dd9419aa29a7f38385dc0e357e808b1a
pdjnsk <80638504+pdjnsk@users.noreply.github.com>
2023-06-03 10:58:14 +0530

    Update Car.js

--- PRO-VR-C152-main/js/Car.js
+++ PRO-VR-C152-main/js/Car.js
@@ -4,8 +4,7 @@ AFRAME.registerComponent("car-model", {
     clickCounter: { type: "number", default: 0 }
   },
   init: function() {
-    // Do something when component first attached.
-    this.el.setAttribute("gltf-model", this.data.modelRef);
+  this.el.setAttribute("gltf-model", this.data.modelRef);
     const position = { x: 0, y: 50, z: 80 };
     const rotation = { x: 0, y: -100, z: 0 };
     this.el.setAttribute("position", position);
